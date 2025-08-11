d65375e521e3f3f9164acf89d1ab7ab076130e04
Eduardo Rigon <edukatiau@gmail.com>
2025-08-11 20:19:08 -0300

    init: start spring project

---
+++ src/main/java/br/ifsul/tcc/pediu_ifoi/PediuIfoiApplication.java
@@ -0,0 +1,13 @@
+package br.ifsul.tcc.pediu_ifoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PediuIfoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PediuIfoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifsul/tcc/pediu_ifoi/PediuIfoiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsul.tcc.pediu_ifoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PediuIfoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
