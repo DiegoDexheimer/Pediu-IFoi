7c7244c28177214ea4f01d46353eb39149f5e091
Eduardo Rigon <edukatiau@gmail.com>
2025-08-25 19:26:16 -0300

    refactor: removing usuario class

--- src/main/java/br/ifsul/tcc/pediu_ifoi/domain/entity/Cantina.java
+++ src/main/java/br/ifsul/tcc/pediu_ifoi/domain/entity/Cantina.java
@@ -1,18 +1,36 @@
 package br.ifsul.tcc.pediu_ifoi.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class Cantina extends Usuario{
-    
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cantina")
+public class Cantina {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idCantina;
 
-    //Pedidos: <pedido> List
+    @Column(name = "login", nullable = false, length = 20)
+    protected String login;
+    
+    @Column(name = "senha", nullable = false, length = 20)
+    protected String senha;
 
-    //Estoque: Estoque
+    @Column(name = "nome", nullable = false, length = 30)
+    protected String nome;
+
+    // Pedidos: <pedido> List
+
+    // Estoque: Estoque
 }
--- src/main/java/br/ifsul/tcc/pediu_ifoi/domain/entity/Cliente.java
+++ src/main/java/br/ifsul/tcc/pediu_ifoi/domain/entity/Cliente.java
@@ -5,14 +5,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class Cliente extends Usuario {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cliente")
+public class Cliente{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long idUsuario;
+
+    @Column(name = "login", nullable = false, length = 20)
+    protected String login;
+    
+    @Column(name = "senha", nullable = false, length = 20)
+    protected String senha;
+
+    @Column(name = "nome", nullable = false, length = 30)
+    protected String nome;
 
     @Column(name ="email", unique = true, nullable = false)
     private String email;
