e975afdc29630c7c7a6a484db618d32f100d4aa0
Eduardo Rigon <edukatiau@gmail.com>
2025-08-25 22:46:27 -0300

    fix: correct method name for cadastrarCantina in CantinaController

--- src/main/java/br/ifsul/tcc/pediu_ifoi/controller/CantinaController.java
+++ src/main/java/br/ifsul/tcc/pediu_ifoi/controller/CantinaController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import br.ifsul.tcc.pediu_ifoi.domain.entity.Cantina;
@@ -17,13 +16,13 @@ public class CantinaController {
     private CantinaService cantinaService;
 
     @GetMapping("/cantina/cadastro_cantina")
-    public String cadastraCantina() {
+    public String cadastrarCantina() {
         System.out.println("-> Cadastro de Cantina acessado");
         return "/cantina/cadastro_cantina";
     }
 
     @PostMapping(value = "/cantina/cadastro_cantina", consumes = "application/x-www-form-urlencoded")
-    public String cadastroCantina(@ModelAttribute Cantina cantina) {
+    public String cadastrarCantina(@ModelAttribute Cantina cantina) {
         System.out.println("-> Iniciando cadastro de cantina");
 
         try {
