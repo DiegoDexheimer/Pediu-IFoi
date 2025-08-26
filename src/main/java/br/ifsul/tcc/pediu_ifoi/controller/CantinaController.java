fad29e1f5c00eb93d51179323b8d0cdafcf5137d
Eduardo Rigon <edukatiau@gmail.com>
2025-08-25 22:43:32 -0300

    fix: update cadastroCantina method to handle exceptions properly and improve logging

--- src/main/java/br/ifsul/tcc/pediu_ifoi/controller/CantinaController.java
+++ src/main/java/br/ifsul/tcc/pediu_ifoi/controller/CantinaController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import br.ifsul.tcc.pediu_ifoi.domain.entity.Cantina;
 import br.ifsul.tcc.pediu_ifoi.service.CantinaService;
-
 
 @Controller()
 public class CantinaController {
@@ -22,8 +22,9 @@ public class CantinaController {
         return "/cantina/cadastro_cantina";
     }
 
-    @PostMapping("/cantina/cadastro_cantina")
+    @PostMapping(value = "/cantina/cadastro_cantina", consumes = "application/x-www-form-urlencoded")
     public String cadastroCantina(@ModelAttribute Cantina cantina) {
+        System.out.println("-> Iniciando cadastro de cantina");
 
         try {
             cantina = cantinaService.salvarCantina(cantina);
@@ -32,6 +33,7 @@ public class CantinaController {
             System.out.println("-> Cadastro de Cantina realizado com sucesso");
         } catch (Exception e) {
             System.out.println("-> Erro ao cadastrar Cantina: " + e.getMessage());
+            throw new RuntimeException("Erro ao cadastrar Cantina");
         }
 
         return "redirect:/";
